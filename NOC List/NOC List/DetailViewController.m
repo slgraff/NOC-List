451d1ce5e6ef0a80349ab0d316e8ac6b8cc264d4
slgraff <slgraff@gmail.com>
2016-04-13 08:29:40 -0400

    Initial commit

---
+++ NOC List/NOC List/DetailViewController.m
@@ -0,0 +1,121 @@
+//
+//  DetailViewController.m
+//  NOC List
+//
+//  Created by Ben Gohlke on 1/28/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+//
+// 17. By the empty circle to the left of this IBOutlet, it looks like this property is not connected to its storyboard
+//     object. How do we do that?
+//
+@property (weak, nonatomic) IBOutlet UILabel *coverNameLabel;
+
+//
+// 18. We need properties for the other two labels here so we can reference them in code.
+//
+@property (weak, nonatomic) IBOutlet UILabel *realNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *levelNumLabel;
+
+
+
+// Why is this here?
+// A: Because you love us
+- (void)configureView;
+
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail view
+
+- (void)setAgent:(Agent *)newAgent
+{
+    if (_agent != newAgent)
+    {
+        _agent = newAgent;
+            
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // What is being checked here?
+    // A: We are checking to see if self.agent exists. If true, execute code
+    if (self.agent)
+    {
+        //
+        // 19. We need to set the title of this view to "Agent #", where # is the agent's last name (use the cover name). But
+        //     the Agent object only has a name property with the first and last name in a single string. How do we get just
+        //     the last name?
+        //
+        //     (hint: We did something similar to this in HW 1)
+        //
+        
+        // More complicated way to extract agent last name
+//        // Get agent cover name
+//        NSString *agentName = self.agent.coverName;
+//        
+//        // Separate name into component 'words'
+//        NSArray *nameComponents = [agentName componentsSeparatedByString:@""];
+//        
+//        // Grab last name, i.e. last component, from nameComponents
+//        NSString *lastName = nameComponents.lastObject;
+        
+        // One line solution to extracing agent last name and setting view title
+        self.title = [NSString stringWithFormat:@"Agent %@", [self.agent.coverName componentsSeparatedByString: @" "][1]];
+    
+        //
+        // 20. Once we have the last name of the agent from the code above, how do we set the view's title to the right
+        //     string?
+        //
+        
+        // More verbose way to set title
+//        NSString *viewTitle =  [@"Agent " stringByAppendingString:lastName];
+//        self.title = viewTitle;
+        
+        
+        
+        //
+        // 21. We need to set the three labels in our view to the agent's cover name, real name, and access label.
+        //
+        //     The level label will be a little trickier, because the level property is an NSInteger. We also want that label
+        //     to read "Level #". How do we do that?
+        //
+
+        self.coverNameLabel.text = self.agent.coverName;
+        self.realNameLabel.text = self.agent.realName;
+        self.levelNumLabel.text = [NSString stringWithFormat:@"Level %ld", (long)self.agent.accessLevel];
+        
+    }
+}
+
+- (void)viewDidLoad
+{
+    // What does this do?
+    // A: Executes the viewDidLoad method of the superClass, UIViewController
+    [super viewDidLoad];
+    
+    //
+    // 22. We need to make sure to call the configureView method so the detail view will be populated with the agent's data.
+    //     How do we do that?
+    //
+    
+    [self configureView];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ NOC List/NOC List/MasterViewController.m
@@ -0,0 +1,178 @@
+//
+//  MasterViewController.m
+//  NOC List
+//
+//  Created by Ben Gohlke on 1/28/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "MasterViewController.h"
+#import "DetailViewController.h"
+#import "Agent.h"
+
+@interface MasterViewController ()
+
+@property NSMutableArray *agents;
+
+@end
+
+@implementation MasterViewController
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //
+    // 3. Set the title of the view to "NOC List"
+    //
+    
+    self.title = @"NOC List";
+    
+    
+    //
+    // 4. Initialize the agents array as an NSMutableArray
+    //
+    self.agents = [NSMutableArray array];
+    
+    // Alternate ways to write above
+    // NSMutableArray *agents = [[NSMutableArray alloc] init];
+    // NSMutableArray *agents = [NSMutableArray new];
+
+    //
+    // 5. Call the method loadNocList so the tableview will actually have objects to load into its cells.
+    //
+    
+    [self loadNocList];
+    
+}
+
+- (void)loadNocList
+{
+    // This creates a string with the filepath to the NOC List JSON file.
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"NOC" ofType:@"json"];
+    // This is a built in method that allows us to load a JSON file into native Cocoa objects (NSDictionaries and NSArrays).
+    NSArray *agents = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:filePath] options:NSJSONReadingAllowFragments error:nil];
+    
+    //
+    // 6. Once we have an array of dictionaries, we need to iterate over them and convert them into Agent objects.
+    //
+    //    Type in "forin" below. It should offer code completion for a for-in loop. Just hit enter to accept it.
+    //    Use the "agents" array from above as the array to iterate over. Create an NSDictionary object on the left side
+    //    of the for-in loop. You will use this inside the for loop to create an Agent object.
+    
+    for (NSDictionary* anAgentDictionary in agents) {
+        
+//        Agent* anAgent = [Agent agentWithDictionary:anAgentDictionary];
+//        [self.agents addObject:anAgent];
+        
+        // Alternate way to write in one line
+        [self.agents addObject:[Agent agentWithDictionary:anAgentDictionary]];
+    
+    }
+    
+    //
+    // 7. Now that we have agent objects, call a method to instruct the table to reload its data.
+    //
+    
+    [self.tableView reloadData];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Segues
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    //
+    // 8. We need to set the segue identifier to the same one we used in the segue on the storyboard
+    //
+    
+    // AgentDetail is my segue identifier
+    if ([[segue identifier] isEqualToString:@"AgentDetail"])
+    {
+        //
+        // 9. We need to get an NSIndexPath for the selected cell
+        //
+        UITableViewCell *selectedCell = (UITableViewCell *)sender;
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:selectedCell];
+        
+        //
+        // 10. Now we're going to use the "row" property of the indexPath from above to pull out the associated Agent object
+        //     from the agents array.
+        //
+        Agent *selectedAgent = self.agents[indexPath.row];
+        
+        //
+        // 11. Now we need to send this Agent object to the detail view controller so it know's which agent's info to show.
+        //     See the method call below on the segue object for how to get a handle to the detail view controller.
+        //
+        //     How would we go about setting this agent object?
+        //
+        
+        DetailViewController *myDetailView = [segue destinationViewController];
+        myDetailView.agent = selectedAgent;
+        
+    }
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //
+    // 12. How do we tell the table view how many rows we need?
+    //
+    
+    return [self.agents count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //
+    // 13. We need the tableview to dequeue a cell for us to use. This will either pull an unused cell and give it to us to
+    //     reuse, or it will create a brand new cell. In either case, we need to make sure to set/reset all the UI elements
+    //     so we don't show stale data.
+    //
+    //     The method call below will perform this dequeuing operation. What should we set as the identifier?
+    //
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AgentDetail" forIndexPath:indexPath];
+
+    //
+    // 14. We need to get a handle to the appropriate Agent object. How do we do that? (hint: we've done this already ^)
+    //
+    Agent *anAgent = [self.agents objectAtIndex: indexPath.row];
+    
+    //
+    // 15. The cell needs to show both the cover name and the real name of the agent. Since we are using one of the built-in
+    //     cell types, the "cell" object above has properties for these two labels already. How do we assign those?
+    //
+
+    cell.textLabel.text = anAgent.coverName;
+    cell.detailTextLabel.text = anAgent.realName;
+    
+    
+    //
+    // 16. This method is supposed to give a cell back to its caller. How do we do that? Why is this method currently
+    //     throwing an error?
+    //
+    
+    return cell;
+    
+}
+
+@end
